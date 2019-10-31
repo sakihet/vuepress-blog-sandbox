ee67e72ba1a88f829d698daf21ec989e05a44831
saki <sakihet@gmail.com>
2019-11-01 01:59:41 +0900

    add pagination to IndexPost

--- .vuepress/theme/layouts/IndexPost.vue
+++ .vuepress/theme/layouts/IndexPost.vue
@@ -44,6 +44,18 @@
         slot="bottom"
       />
     </Page>
+
+    <div>
+      <ul>
+        <li v-for="page in $pagination.pages">
+          <router-link :to="page.path">{{ page.title }}</router-link>
+        </li>
+      </ul>
+      <div>
+        <router-link v-if="$pagination.hasPrev" :to="$pagination.prevLink">Prev</router-link>
+        <router-link v-if="$pagination.hasNext" :to="$pagination.nextLink">Next</router-link>
+      </div>
+    </div>
   </div>
 </template>
 
