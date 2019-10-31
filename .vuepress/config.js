d18375a62670e5e7f0ab957a37b3be09ae76365f
saki <sakihet@gmail.com>
2019-10-31 09:14:16 +0900

    add .vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  plugins: [
+    [
+      '@vuepress/blog',
+      {
+        directories: [
+          {
+            // Unique ID of current classification
+            id: 'post',
+            // Target directory
+            dirname: '_posts',
+            // Path of the `entry page` (or `list page`)
+            path: '/',
+          },
+        ],
+      },
+    ],
+  ],
+}
