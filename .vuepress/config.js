e5adefd67de8d8e04de11e05578cd7c82c4f79e7
saki <sakihet@gmail.com>
2019-11-01 09:07:01 +0900

    add tag feature

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -18,7 +18,29 @@ module.exports = {
             },
           },
         ],
+        frontmatters: [
+          {
+            // Unique ID of current classification
+            id: 'tag',
+            // Decide that the frontmatter keys will be grouped under this classification
+            keys: ['tag'],
+            // Path of the `entry page` (or `list page`)
+            path: '/tag/',
+            // Layout of the `entry page`
+            layout: 'Tags',
+            // Layout of the `scope page`
+            scopeLayout: 'Tag'
+          },
+        ],
       },
     ],
   ],
+  themeConfig: {
+    nav: [
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+    ],
+  },
 }
